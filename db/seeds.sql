98930b2bc086f15f20b223ca4d0cbbff066314b4
Hans <romanghans@gmail.com>
2019-01-24 23:00:15 -0700

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,2 @@
+INSERT INTO burgers (burger_name) VALUES ('Tasty Burger');
+INSERT INTO burgers (burger_name) VALUES ('Meaty Burger');
---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+var routes = require("./controllers/burgers_controller.js");
+
+var port = process.env.PORT || 3000;
+
+var app = express();
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Override with POST having ?_method=DELETE
+app.use(methodOverride("_method"));
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.use("/", routes);
+
+app.listen(port);
